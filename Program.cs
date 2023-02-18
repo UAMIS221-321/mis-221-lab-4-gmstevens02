c0e1e5c68824528d4e168dc5b7bcd6573ee15e57
gmstevens02 <gmstevens@crimson.ua.edu>
2023-02-17 22:17:26 -0600

    push

--- Program.cs
+++ Program.cs
@@ -1,15 +1,18 @@
 ﻿// start main
 using System;
 int userChoice = GetUserChoice(); //priming
-
+do 
+{
+    RouteEm(userChoice);
+    userChoice = GetUserChoice();
+} while (userChoice !=3);
 //end main
 
 //GetUserChoice
 static int GetUserChoice()
 {
-    int userChoice = 0;
-
     DisplayMenu();
+    string userChoice = Console.ReadLine();
     if(IsValidChoice(userChoice)){
         return int.Parse(userChoice);
     }
@@ -18,17 +21,19 @@ static int GetUserChoice()
 }
 
 //DisplayMenu
-static void DisplayMenu()
+static void DisplayMenu(int userChoice)
 {
+    while (userChoice != 3){
     Console.Clear();
-    System.Console.WriteLine("Enter 1 to diplay full triangle\nEnter 2 to diplay partial triangle\nEnter 3 to exit");
+    System.Console.WriteLine("Enter 1 to display full triangle\nEnter 2 to display partial triangle\nEnter 3 to exit");
 
+    }
 }
 
 //IsValidChoice
 static bool IsValidChoice(string userInput)
 {
-    if(userInput=="1"  userInput=="2"  userInput=="3" ){
+    if(userInput=="1" || userInput=="2" || userInput=="3" ){
         return true;
     }
     return false;
@@ -36,20 +41,69 @@ static bool IsValidChoice(string userInput)
 }
 
 //work the menus
+//GetFull
  static void GetFull()
  {
-     int number, i, j, count = 1;
-     Console.Write("Enter ");
+     Random rnd = new Random();
+     int number = rnd.Next(3, 10); //should generate random rows
+     for(int e = 1; e <= number; e++) {
+        for(int g = 1; g <= e; g++){
+            System.Console.WriteLine("* ");
+        System.Console.WriteLine();
+        }
+     }
+     PauseAction();
 }
 
+//GetPartial
  static void GetPartial()
  {
      Random rnd = new Random();
-     System.Console.Write(o);
+     int rows = rnd.Next(3, 10);
+     for(int e = 1; e <= rows; e++) {
+        for(int g = 1; g <= e; g++) {
+            if(g == 1 || e == g || e == rows) {
+                System.Console.WriteLine("* ");
+            }
+            else {
+                int number = new Random().Next(2);
+                if(number == 0) {
+                    System.Console.WriteLine("* ");
+                }
+            else {
+                System.Console.WriteLine(" ");
+            }
+            }
+        }
+        System.Console.WriteLine();
+    }
+    PauseAction();
  }
 
+//SayInvalid
  static void SayInvalid()
  {
-     System.Console.WriteLine("Invalid, try another number");
+     System.Console.WriteLine("Invalid, try another input");
+     PauseAction();
 }
 
+//RouteEm
+static void RouteEm(int menuChoice) 
+{
+    if(menuChoice==1){
+        GetFull();
+    }
+    else if(menuChoice==2){
+        GetPartial();
+    }
+    else if(menuChoice!=3){
+        SayInvalid();
+    }
+}
+
+//The Pause thing
+static void PauseAction()
+{
+    System.Console.WriteLine("Press any key to continue:");
+    Console.ReadKey();
+}
